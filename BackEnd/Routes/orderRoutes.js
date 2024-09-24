b7ca5163f4f4bb64cc00baa078cf10bc60c7f1e6
Mohamed Ben Hafsa <mohamedbenhafsa90@gmail.com>
2024-09-24 15:32:19 +0100

    update protection

--- BackEnd/Routes/orderRoutes.js
+++ BackEnd/Routes/orderRoutes.js
@@ -1,9 +1,6 @@
 import express from "express";
-import {
-    createOrder } from '../Controllers/orderController.js';
+import { createOrder } from "../Controllers/orderController.js";
 
-    import { protect } from "../Middlewares/authMiddleware.js";
-
-    const router = express.Router();
-    router.post("/", protect, createOrder);  
-    export default router;
+const router = express.Router();
+router.post("/", createOrder);
+export default router;
