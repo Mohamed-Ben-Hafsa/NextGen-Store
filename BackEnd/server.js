ac1407ee6355060a5c72853dbe407f18dad68c60
Mohamed Ben Hafsa <mohamedbenhafsa90@gmail.com>
2024-09-24 11:45:36 +0100

    updating server js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,37 +3,41 @@ import dotenv from "dotenv";
 import connectDB from "./Config/db.js";
 import userRoutes from "./Routes/userRoutes.js";
 import CategoryRoutes from "./Routes/categoryRoutes.js";
-
+import cartRoutes from "./Routes/cartRoutes.js";
 import orderRoutes from "./Routes/orderRoutes.js";
 import ProductRoutes from "./Routes/productRoutes.js";
+
 import { errorHandler, notFound } from "./Middlewares/errorMiddleware.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import wishlistRoutes from "./Routes/wishlistRoutes.js";
-import cartRoutes from "./Routes/cartRoutes.js";
 
 const app = express();
 dotenv.config();
 connectDB();
 const PORT = process.env.PORT || 4000;
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:3000", "https://nextgen-store.netlify.app/"],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.get("/", (req, res) => {
+  console.log("Root route accessed");
+  res.status(200).json({ message: "welcom to the Api " });
+});
+
+//Routes
 app.use("/api/users", userRoutes);
 app.use("/api/category", CategoryRoutes);
 app.use("/api/product", ProductRoutes);
-
-app.use("/api/order", orderRoutes);
-app.use("/api/wishlist", wishlistRoutes);
 app.use("/api/cart", cartRoutes);
-
+app.use("/api/order", orderRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "welcom To the Store" });
-});
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
