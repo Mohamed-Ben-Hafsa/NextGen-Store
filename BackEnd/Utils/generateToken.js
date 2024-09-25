469d50a10788cce0b597da50859af4bd52637e53
Mohamed Ben Hafsa <mohamedbenhafsa90@gmail.com>
2024-09-25 12:44:07 +0100

    correcting token

--- BackEnd/Utils/generateToken.js
+++ BackEnd/Utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (res, userId) => {
   res.cookie("JWT", token, {
     httpOnly: true,
     sameSite: "none",
-    secure: true,
+    secure: process.env.NODE_ENV !== "development",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
